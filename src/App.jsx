587738d07159a539752f83be167adf1154ce4208
Goga Pipia <tskali0zamboni@gmail.com>
2024-07-25 01:32:18 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import { useState } from 'react'
-import { puppyList } from './data.js'
+import { useState } from 'react';
+import { puppyList } from './data.js';
 
 function App() {
   console.log(puppyList)
-  const [puppies, setPuppies] = useState(puppyList)
-  
- return (
-    <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
-    
+  {()=>{console.log("puppy id: ", puppy.id)}}
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
+
+    function handleClick() {
+    // some logic here
+  }
+
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p  onClick={() => {console.log("puppy id: ", puppy.id)}} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
     </div>
-  )
+  );
 }
 
 export default App
