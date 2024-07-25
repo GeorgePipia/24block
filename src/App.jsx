e49b075e5057a6952acae55ec41cf8347b02de71
Goga Pipia <tskali0zamboni@gmail.com>
2024-07-25 01:28:44 -0400

    starter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState } from 'react'
+import { puppyList } from './data.js'
 
 function App() {
+  console.log(puppyList)
+  const [puppies, setPuppies] = useState(puppyList)
+  
+ return (
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    
 
-
-  return (
-    <>
-      <div>
-      <h1> Hello </h1>
-      </div>
-    </>
+    </div>
   )
 }
 
